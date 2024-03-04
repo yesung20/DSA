79443473188a99e6a31d00e4adc873559875d590
yesung20 <155213489+yesung20@users.noreply.github.com>
2024-03-05 01:38:20 +0900

    Create BfsTest.java

---
+++ dfs & bfs/BfsTest.java
@@ -0,0 +1,32 @@
+package com.algorithm;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BfsTest {
+    public static void main(String[] args) {
+        boolean[] visited = new boolean[9];
+        int[][] graph = {{}, {2, 3, 8}, {1, 6, 8}, {1, 5}, {5, 7}, {3, 4, 7}, {2}, {4, 5}, {1, 2}};
+        System.out.println(bfs(1, graph, visited));
+    }
+
+    static String bfs(int start, int[][] graph, boolean[] visited){
+        StringBuilder sb = new StringBuilder();
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        visited[start] = true;
+
+        while(!q.isEmpty()){
+            int nodeIndex = q.poll();
+            sb.append(nodeIndex + " -> ");
+            for(int node : graph[nodeIndex]){
+                if(!visited[node]){
+                    visited[node] = true;
+                    q.offer(node);
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+}
